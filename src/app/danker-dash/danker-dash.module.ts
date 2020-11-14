4e227f388ee6257a09a7e2528e5f8f5e3122c6fd
SpencerHehl <schehl49@gmail.com>
2020-11-14 13:23:33 -0500

    danker dash and routing

--- src/app/danker-dash/danker-dash.module.ts
+++ src/app/danker-dash/danker-dash.module.ts
@@ -4,13 +4,15 @@ import { DashMainComponent } from './dash-main/dash-main.component';
 import { DankerRecentsComponent } from './danker-recents/danker-recents.component';
 import { TopDankerComponent } from './top-danker/top-danker.component';
 import { TopDankeeComponent } from './top-dankee/top-dankee.component';
+import { DankerDashRoutingModule } from './danker-dash-routing.module';
 
 
 
 @NgModule({
   declarations: [DashMainComponent, DankerRecentsComponent, TopDankerComponent, TopDankeeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DankerDashRoutingModule,
   ]
 })
 export class DankerDashModule { }
--- src/app/danker-dash/danker-service.service.ts
+++ src/app/danker-dash/danker-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Dank } from './dank.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DankerServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getDanks() {
+  }
+
+  submitDank() {
+  }
 }
